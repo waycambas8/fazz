888b07d1ba25830915eadb027244f6cefe3cd0b3
waycambas8 <waycambas8@gmail.com>
2024-07-18 10:25:58 +0700

    init

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('', function () {
-    return 'Beep Beep server is Online';
+    return 'Beep Beep server is Online Jenkins testing';
 });
 
 Route::get('list/payment', [PaymentBrowseController::class, 'List']);
